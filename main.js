4e4a2be413a72cab9ed31c493002efe3a164fd6d
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-08-30 21:29:38 +0530

    2.2

--- main.js
+++ main.js
@@ -23,4 +23,37 @@ describe(`${Person.name} Class`, () => {
       expect(model.middleName).toBe("");
     });
   });
+
+  describe("full name", () => {
+    beforeEach(() => {
+      model = new Person({
+        firstName: "Dylan",
+        lastName: "Israel",
+      });
+    });
+
+    it("should give firstName and lastName only if middleName is empty", () => {
+      //arrange
+      model.middleName = "";
+
+      //act
+      const expectedResult = `${model.firstName} ${model.lastName}`;
+      const actualResult = model.fullName;
+
+      //assert
+      expect(actualResult).toEqual(expectedResult);
+    });
+
+    it("should give middleName first letter also if middle name is not empty", () => {
+      //arrange
+      model.middleName = "Christopher";
+
+      //act
+      const expectedResult = `${model.firstName} ${model.middleName[0]}. ${model.lastName}`;
+      const actualResult = model.fullName;
+
+      //assert
+      expect(actualResult).toEqual(expectedResult);
+    });
+  });
 });
