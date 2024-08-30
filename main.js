3da0b5aa229588e5702d59c771ff66c7803fba87
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-08-30 21:06:40 +0530

    2.1

--- main.js
+++ main.js
@@ -2,42 +2,23 @@
  * Test Suite
  */
 describe(`${Person.name} Class`, () => {
+  let model;
+  beforeEach(() => {
+    model = new Person();
+  });
+
   describe("Default Values", () => {
     it("should default first name to empty string if not given in constructor", () => {
-      //arrange
-      const data = {
-        firstName: null,
-      };
-
-      //act
-      const model = new Person(data);
-
       // assert
       expect(model.firstName).toBe("");
     });
 
     it("should default last name to empty string if not given in constructor", () => {
-      //arrange
-      const data = {
-        lastName: null,
-      };
-
-      //act
-      const model = new Person(data);
-
       // assert
       expect(model.lastName).toBe("");
     });
 
     it("should default middle name to empty string if not given in constructor", () => {
-      //arrange
-      const data = {
-        middleName: null,
-      };
-
-      //act
-      const model = new Person(data);
-
       // assert
       expect(model.middleName).toBe("");
     });
